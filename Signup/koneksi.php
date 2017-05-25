64975d6ababe2e21509863eab44a0e8b74f5c5a9
opyfitanandakirana <opykirana@gmail.com>
2017-05-25 19:35:12 +0700

    perubahan

--- Signup/koneksi.php
+++ Signup/koneksi.php
@@ -1,13 +1,50 @@
-<?php
+<?php 
 
-	$server   = "localhost";
-	$database = "5-in-1-delivery";
-	$username = "root";
-	$password = "";
+class Connection {
 
-	$koneksi = new mysqli ($server, $username, $password, $database);
+	public $con;
 
-	if($koneksi->connect_error){
-		die("Koneksi gagal");
+	// connection to database
+	public function __construct()
+	{
+		$this->con = new mysqli("localhost","root","","5-in-1-delivery");
 	}
-?>
+
+	// try authentication user
+	public function login($username,$password)
+	{
+		$query="select *From daftar where username=? AND password=?";
+		$stmt=$this->con->prepare($query);
+		$stmt->bind_param("ss",$username,md5($password)); 
+		$stmt->execute();
+
+		if($stmt->fetch())
+		{
+			header("location: index.html");
+		}else {
+			echo"Maaf Anda Gagal Login";
+		}
+	}
+
+	public function register($username,$email,$password,$konfirmasi_password,$status)
+	{
+		if($password==$konfirmasi_password)
+		{
+			$password=md5($password);
+			
+		 	$query="INSERT INTO daftar (username,email,password,status) VALUES(?,?,?,?)";
+		 	$statement=$this->con->prepare($query);
+		 	$statement->bind_param('ssss', $username,$email,$password,$status);
+
+			if($statement->execute())
+			{
+				header("location: login.html");
+			}
+
+		}else {
+			echo 'the password mismatch';
+		}
+
+	}
+
+}
